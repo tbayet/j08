ab2d123774d00ebf180b70dfc556064132131af1
lbougero42 <lbougero@student.42.fr>
2017-01-20 19:56:37 +0100

    place

--- init.php
+++ init.php
@@ -2,19 +2,19 @@
 	$p1 = new Player("blue");
 	$p2 = new Player("red");
 
-	$p1->addShip(new Tie("Marion", , , 0));
-	$p1->addShip(new Tie("Marine", , , 0));
-	$p1->addShip(new Tie("Jean-Marie", , , 0));
-	$p1->addShip(new Naboo("Soral", , , 0));
-	$p1->addShip(new Naboo("Zemmour", , , 0));
-	$p1->addShip(new Destroyer("Trump", , , 0));
+	$p1->addShip(new Tie("Marion",14 ,10 , 0));
+	$p1->addShip(new Tie("Marine",14 ,4 , 0));
+	$p1->addShip(new Tie("Jean-Marie", 14,7 , 0));
+	$p1->addShip(new Naboo("Soral",9 , 4, 0));
+	$p1->addShip(new Naboo("Zemmour",9 ,10 , 0));
+	$p1->addShip(new Destroyer("Trump",6 ,7 , 0));
 
-	$p1->addShip(new Tie("Zaz", , , 180));
-	$p1->addShip(new Tie("Cheminade", , , 180));
-	$p1->addShip(new Tie("Mandela", , , 180));
-	$p1->addShip(new Naboo("Lenine", , , 180));
-	$p1->addShip(new Naboo("Melenchon", , , 180));
-	$p1->addShip(new Destroyer("Gandhi", , , 180));
+	$p2->addShip(new Tie("Zaz",136 ,90 , 180));
+	$p2->addShip(new Tie("Cheminade",136 ,96 , 180));
+	$p2->addShip(new Tie("Mandela",136 ,93 , 180));
+	$p2->addShip(new Naboo("Lenine",151 ,96 , 180));
+	$p2->addShip(new Naboo("Melenchon",151 ,90 , 180));
+	$p2->addShip(new Destroyer("Gandhi",144 ,93 , 180));
 
 	$p1->setNext($p2);
 	$p2->setNext($p1);
